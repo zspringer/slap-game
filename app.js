98b49242fe077d5a70ed3b326cc1734a9729c9a1
Zach Springer <zspringer.springer@gmail.com>
2017-07-30 08:20:19 -0600

    fixed structure to now sum and allow modifiers to attacks

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 
-
 var Player = function (name) {
     this.name = name;
     //this.modifier = modifier;//use the addMods section
@@ -12,16 +11,9 @@ var Player = function (name) {
     };
     this.health = 100;
     this.hits = 0;
-    this.addMods = function () {
-        var modifierTotal = 0;
-        for (var i = 0; i < this.items.length; i++) {
-            var item = this.items[i];
-            modifierTotal += item.modifier;
-        }
-        return modifierTotal
-    }
+
 }
-var target = new Player("Kenny Powers")
+var kenny = new Player("Kenny Powers")
 //console.log(target)
 
 //_______________________________________________________________
@@ -30,7 +22,7 @@ var Item = function (name, modifier, description){
     this.name = name;
     this.modifier = modifier;
     this.description = description;
-} 
+}
 
 //this is the object that holds the arguments that will get passed to the constructor
 var items = {
@@ -39,8 +31,69 @@ var items = {
     sword: new Item("Sword", 0.4, "This is an awesome sword!"),
 }
 
+var mod = []
 
-console.log(target)
+function sumMods() {
+    sum = 0;
+    for (var i = 0; i < mod.length; i++){
+        sum += mod[i]
+    } return sum
+    document.write('Total amount of modifiers is ' + sum)
+}
+
+function damage() {
+    if (sumMods() > 0) {
+    kenny.health += ((kenny.attackType.slap) * sumMods());
+    } else {
+       kenny.health += kenny.attackType.slap; 
+    }
+    kenny.hits += 1;
+    update();
+}
+
+/*function spoof(attackType, mod){
+    kenny.health += (attackType * total)
+    kenny.hits += 1
+    update();
+}*/
+
+/*function calculateModifiers() {
+    for (var i = 0; sum = 0, i < this.items.length; sum +=[i++]);
+    for (var i = 0; i < this.items.length; i++) {
+        var item = this.items[i].modifier;
+        modifierTotal += item[modifier];
+    
+    return sum
+}*/
+
+
+/*function damage(attackType) {
+    //target.health += attackType;
+    kenny.health += attackType * calculateModifiers();
+    //debugger
+    kenny.hits += 1
+    //alert(health)
+    update();
+}*/
+
+/*function giveShield() {
+    kenny.items.push(items.shield)
+}*/
+
+function giveShield() {
+    var shield = 0.3
+    mod.push(shield)
+}
+
+function giveBomb() {
+    var bomb = 0.9
+    mod.push(bomb)
+}
+
+function giveSword() {
+    var sword = 0.4
+    mod.push(sword)
+}
 
 //console.log(items)
 //target.items.push(items.shield, items.bomb, items.sword)
@@ -48,32 +101,24 @@ console.log(target)
 
 //________________________________________________________________
 
-function damage(attackType) {
-        //target.health += attackType;
-        target.health += attackType * target.addMods();
-        //debugger
-        target.hits += 1
-        //alert(health)
-        update();
-}
+
 
 
 //target is actually the "player" gets passed through the player function
 //to access the properties of the player, you need to use "target" as that is what variable I set the individual player to
 document.getElementById("button-container").innerHTML = `
-        <button class="button" type="button" onClick="damage(${target.attackType.slap})">Slap</button>
-        <button class="button" type="button" onClick="damage(${target.attackType.punch})">Punch</button>
-        <button class="button" type="button" onClick="damage(${target.attackType.kick})">Kick</button>
-        <button class="button" type="button" onClick="damage(${modifierPush})">Shield</button>
-        <button class="button" type="button" onClick="damage(${modifierPush})">Bomb</button>
-        <button class="button" type="button" onClick="damage(${modifierPush})">Sword</button>
+        <button class="button" type="button" onClick="damage(${kenny.attackType.slap})">Slap</button>
+        <button class="button" type="button" onClick="damage(${kenny.attackType.punch})">Punch</button>
+        <button class="button" type="button" onClick="damage(${kenny.attackType.kick})">Kick</button>
+        <button class="button" type="button" onClick="giveShield()">Shield</button>
+        <button class="button" type="button" onClick="giveBomb()">Bomb</button>
+        <button class="button" type="button" onClick="giveSword()">Sword</button>
 `
 
 
 //responsible for updating the user interface whenever a value changes
 function update() {
-    document.getElementById("health").innerHTML = target.health
-    console.log(target.health)
-    document.getElementById("name").innerHTML = target.name
-    document.getElementById("hits").innerHTML = target.hits
+    document.getElementById("health").innerHTML = kenny.health
+    document.getElementById("name").innerHTML = kenny.name
+    document.getElementById("hits").innerHTML = kenny.hits
 }
